b18fafd4a8b4d5ec4df86ada7f31245006d253f3
DeTuksa <emmydave414@gmail.com>
2024-04-08 16:12:17 +0100

    Freestanding rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// main.rs
+
+#![no_std] //Unlink the standard library
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle] // To ensure the Rust compiler outputs a function with the name _start
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
