82c881fe90add995a6b9d3db346e145dffcd3d29
DeTuksa <emmydave414@gmail.com>
2024-04-09 18:00:13 +0100

    Creating a bootimage

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,17 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // To ensure the Rust compiler outputs a function with the name _start
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
     loop {}
 }
