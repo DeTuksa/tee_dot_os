7485732a81d2b587e4b9c77df230495967c1d09d
DeTuksa <emmydave414@gmail.com>
2024-05-19 12:35:50 +0100

    Feat: VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,18 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 // This function is called on panic
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // To ensure the Rust compiler outputs a function with the name _start
-pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
     loop {}
 }
