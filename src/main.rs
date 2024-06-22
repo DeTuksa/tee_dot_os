2ef96bef6fb30d77d78ea22447026a07c5fb8f2b
DeTuksa <emmydave414@gmail.com>
2024-06-22 13:44:26 +0100

    Add: lib and test

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,22 @@
 #![no_std] //Unlink the standard library
 #![no_main] // Disable all Rust-level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(tee_dot_os::test_runner)]
 
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use tee_dot_os::println;
 
-mod vga_buffer;
-mod serial;
+#[no_mangle] // To ensure the Rust compiler outputs a function with the name _start
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
 
 // This function is called on panic
 #[cfg(not(test))]
@@ -23,63 +31,10 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
+    tee_dot_os::test_panic_handler(info)
 }
 
-#[no_mangle] // To ensure the Rust compiler outputs a function with the name _start
-pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
-    test_main();
-
-    loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-     {
-    fn run(&self) -> () {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Testable]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
+// #[test_case]
+// fn trivial_assertion() {
+//     assert_eq!(1, 1);
+// }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use tee_dot_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop {}
+}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
+}
