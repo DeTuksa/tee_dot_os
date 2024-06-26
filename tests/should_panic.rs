fe9d968a3512b4aca565caa93d9fdbefa547935a
DeTuksa <emmydave414@gmail.com>
2024-06-26 12:48:31 +0100

    Test: integration test

---
+++ tests/should_panic.rs
@@ -0,0 +1,27 @@
+//  tests/should_panic.rs
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use tee_dot_os::{QemuExitCode, exit_qemu, serial_println, serial_print};
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    serial_println!("[ok]");
+    exit_qemu(QemuExitCode::Success);
+    loop {}
+}
+
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
