6c3b50f154e2598640ec1607499de45efad12303
DeTuksa <emmydave414@gmail.com>
2024-06-26 15:49:42 +0100

    Add: Double fault handle

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -9,6 +9,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -21,4 +22,15 @@ extern "x86-interrupt" fn breakpoint_handler(
     stack_frame: InterruptStackFrame
 ) {
     println!("EXCEPTION: BREAKPOINT\n{:#?} ", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame, _error_code: u64
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+#[test_case]
+fn test_breakpoint_exception() {
+    x86_64::instructions::interrupts::int3();
 }
--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,9 @@ pub extern "C" fn _start() -> ! {
 
     tee_dot_os::init();
 
-    x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u8) = 42;
+    };
 
     #[cfg(test)]
     test_main();
